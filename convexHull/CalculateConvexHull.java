11ed72f58f794c28b2b136047945841b74128cdd
ajay <aamodi@asu.edu>
2015-11-07 19:12:09 -0700

    fixing convexhull output and adding delete files if exist

--- convexHull/CalculateConvexHull.java
+++ convexHull/CalculateConvexHull.java
@@ -4,11 +4,14 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -71,6 +74,23 @@ public class CalculateConvexHull
 		}
 	};
 	
+	private static List<Coordinate> sortList(List<Coordinate> cr){
+		Collections.sort(cr,new Comparator<Coordinate>() {
+
+	    	public int compare(Coordinate o1, Coordinate o2) {
+	    		if(o1==null || o2==null){
+	    			return 0;
+	    		}
+	    		else if(o1.y!=o2.y){
+	    			return Double.compare(o1.y, o2.y);
+	    		}else{
+	    			return Double.compare(o1.x, o2.x);
+	    		}
+	    	}
+    	});
+
+		return cr;
+	}
 	
     public static void main( String[] args ) throws FileNotFoundException
     {
@@ -81,11 +101,23 @@ public class CalculateConvexHull
 		}
 
     	String inputFile = args[0];
+    	//setting up default partition if not given in argument
     	int noOfPartitions = 3;
     	if(args.length == 3) {
     		noOfPartitions = Integer.parseInt(args[2]);
     	}
     	
+    	//delete if output file already exist
+		try{
+    		File file = new File(args[1]);
+    		if(file.delete()){
+    			System.out.println(file.getName() + " is deleted!");
+    		}else{
+    			System.out.println("Delete operation is failed.");
+    		}
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}
     	
     	SparkConf conf = new SparkConf().setAppName("convexHull").setMaster("local");
     	JavaSparkContext sc = new JavaSparkContext(conf);
@@ -97,8 +129,17 @@ public class CalculateConvexHull
     	// Map each String in the file as a coordinate object
     	JavaRDD<Coordinate> coordinates = inputData.map(parseData);//.repartition(noOfPartitions);
     	
+    	// Sort coordinates before giving it to calculate Convex Hull, this is required to get optimized result.
+    	List<Coordinate> cr = coordinates.collect();
+    	
+    	// sort coordinates before mapping
+    	cr = sortList(cr);
+    	// Convert sorted collection to RDD
+		JavaRDD<Coordinate> sortedRDD = sc.parallelize(cr);
+
+    	
     	// Perform Convex hull operation on individual partition
-    	JavaRDD<Coordinate> localHull = coordinates.mapPartitions(new hull());
+    	JavaRDD<Coordinate> localHull = sortedRDD.mapPartitions(new hull());
     	
     	// Repartition to 1 partition in order to apply 'convex hull' on all the Coordinate objects obtained from individual partitions
     	JavaRDD<Coordinate> calculatedHull = localHull.coalesce(1).cache();
@@ -114,6 +155,10 @@ public class CalculateConvexHull
 			e.printStackTrace();
 		}
     	PrintWriter out = new PrintWriter(args[1]);
+    	//since polygon finishes at the same point, last coordinate and first coordinate will be same, hence removing it. 
+    	convexHullList.remove(convexHullList.size()-1);
+    	// sort coordinates before outputing
+    	convexHullList = sortList(convexHullList);
         for (Coordinate crd : convexHullList) {
             out.println(crd.x+","+crd.y);
         }
